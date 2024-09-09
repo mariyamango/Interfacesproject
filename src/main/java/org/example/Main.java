362df5c8bae74846066e4ec9656ab87420e9c41a
mariyamango <de.msomkova@gmail.com>
2024-09-09 10:18:32 +0200

    Cleaned up the code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,3 @@
-//Step 1: Define an interface 'Playable' that contains a method 'play' without implementation.
-//Step 2: Create a class 'MusicPlayer' that implements the 'Playable' interface. Implement the 'play' method to play a song.
-//Step 3: Create a class 'VideoPlayer' that also implements the 'Playable' interface. Implement the 'play' method to play a video.
-//Step 4: Create a class 'MediaController' that contains a method 'playMedia'.
-// This method should accept a 'Playable' object as a parameter and invoke the 'play' method.
-//Step 5: In the 'main' method, initialize an object of the 'MusicPlayer' and 'VideoPlayer' classes with the 'Playable' interface (polymorphism).
-//Step 6: In the 'main' method, create an object of the 'MediaController' class and use it to play both a song and a video.
-
 package org.example;
 
 public class Main {
@@ -13,11 +5,9 @@ public class Main {
 
         Playable musicPlayer = new MusicPlayer();
         Playable videoPlayer = new VideoPlayer();
-        MusicPlayer musicPlayer1 = new MusicPlayer();
 
         MediaController mediaController = new MediaController();
         mediaController.playMedia(musicPlayer);
         mediaController.playMedia(videoPlayer);
-        mediaController.playMedia(musicPlayer1);
     }
 }
